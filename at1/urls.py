fcecc2bbe3f9888872510735bab14127a42a82c6
GStCl <Genevieve.StClair@student.uts.edu.au>
2024-03-20 19:20:34 +1100

    Version 1.2

--- at1/urls.py
+++ at1/urls.py
@@ -5,3 +5,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('eduprod/', include('eduprod.urls')),
 ]
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
+    path('eduprod/', include(('eduprod.urls', 'eduprod'), namespace='eduprod')),
+    path('accounts/login/', include('users.urls')),
+]
